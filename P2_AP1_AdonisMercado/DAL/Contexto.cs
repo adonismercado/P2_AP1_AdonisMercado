46a6a4ef9b6f6be6f9278aae7cfce741be45172a
adonismercado <adonis_mercado@ucne.edu.do>
2025-10-27 18:36:33 -0400

    Creando el contexto

---
+++ P2_AP1_AdonisMercado/DAL/Contexto.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using P2_AP1_AdonisMercado.Models;
+
+namespace P2_AP1_AdonisMercado.DAL;
+
+public class Contexto : DbContext
+{
+    public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+
+    public DbSet<Registros> Registros { get; set; }
+}
