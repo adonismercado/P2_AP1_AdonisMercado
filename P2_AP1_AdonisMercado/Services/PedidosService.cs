d5dbbe3af7d6ab3a73a6d34744ce0376e2d31e3d
adonismercado <adonis_mercado@ucne.edu.do>
2025-11-03 18:53:29 -0400

    Corrigiendo el service para corregir errores

--- P2_AP1_AdonisMercado/Services/PedidosService.cs
+++ P2_AP1_AdonisMercado/Services/PedidosService.cs
@@ -4,44 +4,44 @@ using P2_AP1_AdonisMercado.Models;
 using System.Linq.Expressions;
 
 namespace P2_AP1_AdonisMercado.Services;
-public class RegistrosService(IDbContextFactory<Contexto> DbFactory)
+public class PedidosService(IDbContextFactory<Contexto> DbFactory)
 {
-    public async Task<bool> Guardar(Pedidos registro)
+    public async Task<bool> Guardar(Pedidos pedido)
     {
-        if (!await Existe(registro.RegistroId))
+        if (!await Existe(pedido.PedidoId))
         {
-            return await Insertar(registro);
+            return await Insertar(pedido);
         }
         else
         {
-            return await Modificar(registro);
+            return await Modificar(pedido);
         }
     }
 
-    public async Task<bool> Existe(int registroId)
+    public async Task<bool> Existe(int pedidoId)
     {
         await using var contexto = await DbFactory.CreateDbContextAsync();
-        return await contexto.Registros
-            .AnyAsync(r => r.RegistroId == registroId);
+        return await contexto.Pedidos 
+            .AnyAsync(r => r.PedidoId == pedidoId);
     }
-    public async Task<bool> Insertar(Pedidos registro)
+    public async Task<bool> Insertar(Pedidos pedido)
     {
         await using var contexto = await DbFactory.CreateDbContextAsync();
-        contexto.Registros.Add(registro);
+        contexto.Pedidos.Add(pedido);
         return await contexto.SaveChangesAsync() > 0;
     }
 
-    public async Task<bool> Modificar(Pedidos registro)
+    public async Task<bool> Modificar(Pedidos pedido)
     {
         await using var contexto = await DbFactory.CreateDbContextAsync();
-        contexto.Registros.Update(registro);
+        contexto.Pedidos.Update(pedido);
         return await contexto.SaveChangesAsync() > 0;
     }
 
     public async Task<List<Pedidos>> Listar(Expression<Func<Pedidos, bool>> criterio)
     {
         await using var contexto = await DbFactory.CreateDbContextAsync();
-        return await contexto.Registros
+        return await contexto.Pedidos
             .Where(criterio)
             .AsNoTracking()
             .ToListAsync();
