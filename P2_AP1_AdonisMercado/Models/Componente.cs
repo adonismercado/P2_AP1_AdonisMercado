db647275286f7fa9cd2fabe750d5d5028ecf1b43
adonismercado <adonis_mercado@ucne.edu.do>
2025-11-03 18:22:46 -0400

    Quitando llabe innecesaria

--- P2_AP1_AdonisMercado/Models/Componente.cs
+++ P2_AP1_AdonisMercado/Models/Componente.cs
@@ -1,15 +1,13 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using System.ComponentModel.DataAnnotations;
 
-namespace P2_AP1_AdonisMercado.Models
+namespace P2_AP1_AdonisMercado.Models;
+
+public class Componente
 {
-    public class Componente
-    {
-        [Key]
-        public int ComponenteId { get; set; }
-        public string Descripcion { get; set; } = string.Empty;
-        public decimal Precio { get; set; }
-        public int Existencia { get; set; }
-    }
-}
+    [Key]
+    public int ComponenteId { get; set; }
+    public string Descripcion { get; set; } = string.Empty;
+    public decimal Precio { get; set; }
+    public int Existencia { get; set; }
 }
