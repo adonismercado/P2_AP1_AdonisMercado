8b9118c3dc1467e3894907d60dfee10d48702a41
adonismercado <adonis_mercado@ucne.edu.do>
2025-11-03 18:18:36 -0400

    Creando el modelo Componente

---
+++ P2_AP1_AdonisMercado/Models/Componente.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace P2_AP1_AdonisMercado.Models
+{
+    public class Componente
+    {
+        [Key]
+        public int ComponenteId { get; set; }
+        public string Descripcion { get; set; } = string.Empty;
+        public decimal Precio { get; set; }
+        public int Existencia { get; set; }
+    }
+}
+}
