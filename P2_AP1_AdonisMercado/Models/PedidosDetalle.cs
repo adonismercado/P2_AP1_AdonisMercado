d70dc101abbdcf8d903cc78a1cdf9b16b20a71f7
adonismercado <adonis_mercado@ucne.edu.do>
2025-11-03 18:44:21 -0400

    Creando el modelo PedidoDetalle

---
+++ P2_AP1_AdonisMercado/Models/PedidosDetalle.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace P2_AP1_AdonisMercado.Models;
+
+public class PedidosDetalle
+{
+    [Key]
+    public int DetalleId { get; set; }
+    public int PedidoId { get; set; }
+    public int ComponenteId { get; set; }
+    public int Cantidad { get; set; }
+    public decimal Precio { get; set; }
+}
