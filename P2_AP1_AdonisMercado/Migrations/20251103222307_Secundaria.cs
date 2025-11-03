5dfb93a5002cf801c1ee327fe4100adf393c4c43
adonismercado <adonis_mercado@ucne.edu.do>
2025-11-03 18:24:04 -0400

    Haciendo la migracion y la base de datos

---
+++ P2_AP1_AdonisMercado/Migrations/20251103222307_Secundaria.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace P2_AP1_AdonisMercado.Migrations
+{
+    /// <inheritdoc />
+    public partial class Secundaria : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Componentes",
+                columns: table => new
+                {
+                    ComponenteId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Descripcion = table.Column<string>(type: "TEXT", nullable: false),
+                    Precio = table.Column<decimal>(type: "TEXT", nullable: false),
+                    Existencia = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Componentes", x => x.ComponenteId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Registros",
+                columns: table => new
+                {
+                    RegistroId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Registros", x => x.RegistroId);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Componentes",
+                columns: new[] { "ComponenteId", "Descripcion", "Existencia", "Precio" },
+                values: new object[,]
+                {
+                    { 1, "Memoria 4GB", 1, 1580m },
+                    { 2, "Disco SSD 120MB", 8, 4200m },
+                    { 3, "Tarjeta de Video", 4, 10000m }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Componentes");
+
+            migrationBuilder.DropTable(
+                name: "Registros");
+        }
+    }
+}
