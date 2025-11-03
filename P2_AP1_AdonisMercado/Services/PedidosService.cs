7456871bbacee515009f6c7253e0700d75ab4802
adonismercado <adonis_mercado@ucne.edu.do>
2025-11-03 19:17:53 -0400

    Modificando el servicio para que cuando se inserte sume la existencia, cuando elimine reste, y cuando modifique haga la sumatoria correcta

--- P2_AP1_AdonisMercado/Services/PedidosService.cs
+++ P2_AP1_AdonisMercado/Services/PedidosService.cs
@@ -28,12 +28,47 @@ public class PedidosService(IDbContextFactory<Contexto> DbFactory)
     {
         await using var contexto = await DbFactory.CreateDbContextAsync();
         contexto.Pedidos.Add(pedido);
+
+        foreach (var detalle in pedido.PedidosDetalles)
+        {
+            var componente = await contexto.Componentes.FirstAsync(c => c.ComponenteId == detalle.ComponenteId);
+            if (componente != null)
+            {
+                componente.Existencia += detalle.Cantidad;
+            }
+        }
+
         return await contexto.SaveChangesAsync() > 0;
     }
 
     public async Task<bool> Modificar(Pedidos pedido)
     {
         await using var contexto = await DbFactory.CreateDbContextAsync();
+
+        var anterior = await contexto.Pedidos
+            .Include(d => d.PedidosDetalles)
+            .AsNoTracking()
+            .FirstOrDefaultAsync(d => d.PedidoId == pedido.PedidoId);
+
+        if (anterior == null)
+        {
+            return false;
+        }
+
+        foreach (var comp in await ListarComponentes())
+        {
+
+            int antes = anterior.PedidosDetalles
+                .Where(p => p.ComponenteId == comp.ComponenteId)
+                .Sum(p => p.Cantidad);
+
+            int ahora = pedido.PedidosDetalles
+                .Where(p => p.ComponenteId == comp.ComponenteId)
+                .Sum(p => p.Cantidad);
+
+            comp.Existencia += (ahora - antes);
+        }
+
         contexto.Pedidos.Update(pedido);
         return await contexto.SaveChangesAsync() > 0;
     }
@@ -43,6 +78,7 @@ public class PedidosService(IDbContextFactory<Contexto> DbFactory)
         await using var contexto = await DbFactory.CreateDbContextAsync();
         return await contexto.Pedidos
             .Where(criterio)
+            .Include(d => d.PedidosDetalles)
             .AsNoTracking()
             .ToListAsync();
     }
@@ -59,6 +95,7 @@ public class PedidosService(IDbContextFactory<Contexto> DbFactory)
         await using var contexto = await DbFactory.CreateDbContextAsync();
 
         var pedido = await contexto.Pedidos
+            .Include(d => d.PedidosDetalles)
             .AsNoTracking()
             .FirstOrDefaultAsync(p => p.PedidoId == pedidoId);
 
@@ -67,7 +104,25 @@ public class PedidosService(IDbContextFactory<Contexto> DbFactory)
             return false;
         }
 
+        foreach (var detalle in pedido.PedidosDetalles)
+        {
+            var componente = await contexto.Componentes.FindAsync(detalle.ComponenteId);
+            if (componente != null)
+            {
+                componente.Existencia -= detalle.Cantidad;
+            }
+        }
+
+        contexto.PedidosDetalles.RemoveRange(pedido.PedidosDetalles);
         contexto.Pedidos.Remove(pedido);
         return await contexto.SaveChangesAsync() > 0;
+    }
+
+    public async Task<List<Componente>> ListarComponentes()
+    {
+        await using var contexto = await DbFactory.CreateDbContextAsync();
+        return await contexto.Componentes
+            .AsNoTracking()
+            .ToListAsync();
     }
 }
